b70d38beaafbeb53c75d2549486ccb371e67bd73
ban <bandvfx17188@funix.edu.vn>
2022-11-03 16:21:04 +0700

    ok

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
-import { combineReducers } from 'redux'
-import { SELECT_REDDIT, REQUEST_POSTS, RECEIVE_POSTS } from '../actions'
+import { combineReducers } from 'redux';
+import { SELECT_REDDIT, REQUEST_POSTS, RECEIVE_POSTS } from '../actions';
 
 function selectedReddit(state = 'reactjs', action) {
   switch (action.type) {
     case SELECT_REDDIT:
-      return action.reddit
+      return action.reddit;
     default:
-      return state
+      return state;
   }
 }
 
@@ -19,7 +19,7 @@ function posts(
 ) {
   switch (action.type) {
     case REQUEST_POSTS:
-      return { ...state, isFetching: true }
+      return { ...state, isFetching: true };
 
     case RECEIVE_POSTS:
       return {
@@ -27,9 +27,9 @@ function posts(
         isFetching: false,
         items: action.posts,
         lastUpdated: action.receivedAt,
-      }
+      };
     default:
-      return state
+      return state;
   }
 }
 
@@ -40,15 +40,15 @@ function postsByReddit(state = {}, action) {
       return {
         ...state,
         [action.reddit]: posts(state[action.reddit], action),
-      }
+      };
     default:
-      return state
+      return state;
   }
 }
 
 const rootReducer = combineReducers({
-  postsByReddit,
   selectedReddit,
-})
+  postsByReddit,
+});
 
-export default rootReducer
+export default rootReducer;
